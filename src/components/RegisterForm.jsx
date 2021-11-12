12795d404db2b135d721a23f8c03f49293cf93fb
Ivan Dominguez <ivanleodomin00@gmail.com>
2021-11-12 11:45:48 -0300

    lrmqlp

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
 import { React, useState } from "react";
-=======
-import { React, useState} from "react";
->>>>>>> 131a919d26b07a5419889b67a432c7aef1814dd3
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import PhoneInput from 'react-phone-number-input'
 
 import {
   Flex,
   Box,
   FormControl,
   FormLabel,
+  FormErrorMessage,
+  FormHelperText,
   Input,
   Checkbox,
   Stack,
@@ -23,41 +23,35 @@ import {
 export default function RegisterForm() {
   const history = useHistory();
 
-<<<<<<< HEAD
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-=======
-  const [user, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAdmin, setIsAdmin] = useState(false);
->>>>>>> 131a919d26b07a5419889b67a432c7aef1814dd3
   const [tel, setTel] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+} = useForm();
+
+    const valUser = /^[a-zA-Z0-9_.-]*$/;
+    const noValUser = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~*]/;
+    const valNum= /[0-9]/g 
+
+  const onSubmit = ({userName,email,password,tel}) => {
     axios
-<<<<<<< HEAD
       .post("/api/auth/register", {
         userName,
-=======
-      .post("http://localhost:8080/api/auth/register", {
-        user,
->>>>>>> 131a919d26b07a5419889b67a432c7aef1814dd3
         email,
         password,
         tel,
       })
       .then((res) => res.data);
-<<<<<<< HEAD
-=======
-    alert("El usuario se ha registrado");
->>>>>>> 131a919d26b07a5419889b67a432c7aef1814dd3
     history.push("/login");
   };
 
   return (
+    <form onSubmit={handleSubmit(onSubmit)}>
     <Flex minH={"100vh"} align={"center"} justify={"center"} bg={"gray.50"}>
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
@@ -65,51 +59,86 @@ export default function RegisterForm() {
         </Stack>
         <Box rounded={"lg"} bg={"white"} boxShadow={"lg"} p={8}>
           <Stack spacing={4}>
-<<<<<<< HEAD
-            <form onSubmit={handleSubmit}>
-              <FormControl id="userName">
-                <FormLabel>User</FormLabel>
+
+              <FormControl id="userName" isInvalid={noValUser.test(userName) && errors.userName} isRequired >
+                <FormLabel htmlFor="name">User</FormLabel>
                 <Input
+                  id="userName"
                   type="text"
+                  {...register('userName', {
+                    required: 'Allow only numbers and characters',
+                    pattern: {
+                        value: valUser,
+                        message: 'Allow Only numbers and characters',
+                    },
+                    minLength: { value: 3, message: 'Minimum length should be 3' },
+                })}
                   value={userName}
                   onChange={(e) => {
                     setUserName(e.target.value);
                   }}
+                  
                 />
+                <FormErrorMessage>{errors.userName && errors.userName.message}</FormErrorMessage>
               </FormControl>
 
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
+
+              <FormControl id="email" isInvalid={errors.email} isRequired>
+                <FormLabel htmlFor="email">Email address</FormLabel>
                 <Input
                   type="email"
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
+                  
+                  {...register('email', {
+                    required: 'Email is Required',
+                    pattern: {
+                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: 'Invalid email address',
+                    },
+                })}
                   value={email}
+                  onChange={(e) => {setEmail(e.target.value)}}
                 />
               </FormControl>
 
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
+              <FormControl id="password" isInvalid={password.length < 5 && errors.password} isRequired>
+                <FormLabel htmlFor="password">Password</FormLabel>
                 <Input
                   type="password"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
+                  {...register('password', {
+
+                    required: 'Password is Required',
+                    pattern:{
+                      minLength: { value: 5},
+                      message: 'Password minimun length should be 5'
+                    }
+                  })}
                   value={password}
+                  onChange={(e) => {setPassword(e.target.value)}}
                 />
+                <FormErrorMessage>{errors.password && errors.password.message}</FormErrorMessage>
               </FormControl>
 
-              <FormControl id="tel">
-                <FormLabel>Telephone</FormLabel>
+              <FormControl id="tel" isInvalid={noValUser.test(tel) && errors.tel} isRequired >
+                <FormLabel htmlFor="name">Telephone</FormLabel>
                 <Input
-                  type="number"
+                  id="tel"
+                  type="text"
+                  {...register('tel', {
+                    required: 'Allow only numbers',
+                    pattern: {
+                        value: valNum,
+                        message: 'Allow Only numbers',
+                    },
+                    minLength: { value: 8, message: 'Minimum length should be 8' },
+                })}
                   value={tel}
                   onChange={(e) => {
                     setTel(e.target.value);
                   }}
+                  
                 />
-              </FormControl>
+                <FormErrorMessage>{errors.tel && errors.tel.message}</FormErrorMessage>
+                </FormControl>
 
               <Stack spacing={10}>
                 <Stack
@@ -131,78 +160,10 @@ export default function RegisterForm() {
                   Sign in
                 </Button>
               </Stack>
-            </form>
-=======
-            <FormControl id="userName">
-              <FormLabel>User</FormLabel>
-              <Input
-                type="text"
-                value={user}
-                onChange={(e) => {
-                  setUser(e.target.value);
-                  console.log(e.target.value);
-                }}
-              />
-            </FormControl>
-
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input
-                type="email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                value={email}
-              />
-            </FormControl>
-
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input
-                type="password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                value={password}
-              />
-            </FormControl>
-
-            <FormControl id="tel">
-              <FormLabel>Telephone</FormLabel>
-              <Input
-                type="number"
-                value={tel}
-                onChange={(e) => {
-                  setTel(e.target.value);
-                  console.log(e.target.value);
-                }}
-              />
-            </FormControl>
-
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: "column", sm: "row" }}
-                align={"start"}
-                justify={"space-between"}
-              >
-                <Checkbox>Remember me</Checkbox>
-                <Link color={"blue.400"}>Forgot-password?</Link>
-              </Stack>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-                onSubmit={handleSubmit}
-              >
-                Sign in
-              </Button>
-            </Stack>
->>>>>>> 131a919d26b07a5419889b67a432c7aef1814dd3
           </Stack>
         </Box>
       </Stack>
     </Flex>
+  </form>
   );
 }
