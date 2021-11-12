0190d13c39019952c8e3beb4a4c347d6535ea936
MatiDifi <difioremati@gmail.com>
2021-11-12 12:25:38 -0300

    fake data copada

---
+++ api/utils/fakedata.js
@@ -0,0 +1,46 @@
+const arrayProducts = [
+    {
+        "name": "Guitarra Electrica",
+        "model": "Cort",
+        "description": "Guitarra electrica para zurdos.Guitarra de diseño clásico con doble cutaway. Cuerpo de tilo. Mástil Bolt-on de arce canadiense. Diapasón de palo rosa. 22 trastes. Escala 25.5. Pastillas Bluebucker Pro BPEG5-F & BPEG5-M & BPANC-2R . configuración H-S-S. Clavijeros Diecasting. Puente Tremolo Wilkinson VS50II. Herrajes cromados.Color Black.",
+        "photo": "https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/d_825525-mla29225876718_012019-o-b667e0581ba64d31e615992359694910-640-0.jpg",
+        "stock": 2,
+        "price": 50000
+    },
+    {
+        "name": "Guitarra Criolla",
+        "model": "Cort",
+        "description": "Guitarra criolla para zurdos.Guitarra de diseño clásico con doble cutaway. Cuerpo de tilo. Mástil Bolt-on de arce canadiense. Diapasón de palo rosa. 22 trastes. Escala 25.5. Pastillas Bluebucker Pro BPEG5-F & BPEG5-M & BPANC-2R . configuración H-S-S. Clavijeros Diecasting. Puente Tremolo Wilkinson VS50II. Herrajes cromados.Color Black.",
+        "photo":"https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/guitarra-electro-criolla-fonseca-40kec-41kec-funda_iz1141063776xvzxxpz1xfz74514398-617309937-1-jpgxsz74514398xim1-abb1dc4f6db4a4b4b515990563207217-640-0.jpg" ,
+        "stock": 2,
+        "price": 50000
+    },
+    {
+        "name": "Piano",
+        "model": "modelo",
+        "description": "Un piano re copado",
+        "photo": "https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/00000000000pa88h65594pa88h-11-001af5bb20a010ef1415997414161911-640-0.jpg" ,
+        "stock": 2,
+        "price": 50000
+    },
+    {
+        "name": "Bajo",
+        "model": "modelo",
+        "description": "Un bajo re copado",
+        "photo": "https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/ddb1-fd82639bc6bac08cae15984660046857-640-0.jpg",
+        "stock": 2,
+        "price": 50000
+    }
+];
+
+const arrayCategory = [
+    {name: "guitarra"},
+    {name: "cuerda"},
+    {name: "percusion"},
+    {name: "amplicadores"},
+    {name: "bajo"},
+    {name: "microfono"},
+    {name: "viento"},
+]
+
+module.exports = {arrayProducts, arrayCategory}
